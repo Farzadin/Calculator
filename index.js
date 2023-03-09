23231f34a9d52dc47b11a7abb22003ece2cafa6f
thisiscyiran <farzad.dianat@gmail.com>
2023-03-09 05:15:44 +0330

    fix the calculator's screen size

--- index.js
+++ index.js
@@ -15,8 +15,14 @@ function divide(a, b) {
 }
 
 function operate(opt, a, b) {
+  a = Number(a);
+  b = Number(b);
+
   if (opt === "+") return add(a, b);
   if (opt === "-") return subtract(a, b);
   if (opt === "×") return multiply(a, b);
+  if (opt === "÷" && b === 0) return null;
   if (opt === "÷") return divide(a, b);
+
+  return null;
 }
