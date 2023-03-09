ce6b29e6650b35a2835e6fd8a6b2a2b70b60bcfe
thisiscyiran <farzad.dianat@gmail.com>
2023-03-09 22:44:49 +0330

    Add functionality to equals button

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const operatorButtons = document.querySelectorAll(".opt");
 const NumberButtons = document.querySelectorAll(".num");
 const clearButton = document.querySelector(".clear");
 const deleteButton = document.querySelector(".remove");
+const equalButton = document.querySelector(".equal");
 
 NumberButtons.forEach((numberButton) => {
   numberButton.addEventListener("click", addNumberIntoScreen);
@@ -20,8 +21,22 @@ operatorButtons.forEach((operatorButton) => {
 
 clearButton.addEventListener("click", clearScreen);
 deleteButton.addEventListener("click", DeleteLatestInput);
+equalButton.addEventListener("click", evaluate);
+
+function evaluate() {
+  if (operator === "") return;
+  secondOperand = currentValueElem.textContent;
+  previousValueElem.textContent = `${secondOperand} ${operator} ${firstOperand} ${equalButton.textContent}`;
+  let result = operate(operator, firstOperand, secondOperand);
+  currentValueElem.textContent = result;
+  resetFlag = true;
+}
 
 function addNumberIntoScreen(event) {
+  if (previousValueElem.textContent.includes("=")) {
+    previousValueElem.textContent = "";
+    resetCurrentValueElem();
+  }
   const newInput = event.target.textContent;
 
   if (currentValueElem.textContent == 0 || resetFlag) resetCurrentValueElem();
