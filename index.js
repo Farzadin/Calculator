7bbb0ffff0e3424e06e801b0cf8750dbad0f1be7
thisiscyiran <farzad.dianat@gmail.com>
2023-04-10 16:26:48 +0330

    Refactor code

--- index.js
+++ index.js
@@ -1,88 +1,38 @@
-let firstOperand = "";
-let operator = "";
-let secondOperand = "";
+let firstOperand = '';
+let operator = '';
+let secondOperand = '';
 let resetFlag = false;
 
-const currentValueElem = document.querySelector(".currentvalue");
-const previousValueElem = document.querySelector(".previousvalue");
-const operatorButtons = document.querySelectorAll(".opt");
-const NumberButtons = document.querySelectorAll(".num");
-const clearButton = document.querySelector(".clear");
-const deleteButton = document.querySelector(".remove");
-const equalButton = document.querySelector(".equal");
-const pointButton = document.querySelector(".point");
-
-NumberButtons.forEach((numberButton) => {
-  numberButton.addEventListener("click", addNumberIntoScreen);
-});
-
-operatorButtons.forEach((operatorButton) => {
-  operatorButton.addEventListener("click", addOperatorIntoScreen);
-});
-
-clearButton.addEventListener("click", clearScreen);
-deleteButton.addEventListener("click", DeleteLatestInput);
-equalButton.addEventListener("click", evaluate);
-pointButton.addEventListener("click", addDecimalIntoScreen);
+const currentValueElem = document.querySelector('.currentvalue');
+const previousValueElem = document.querySelector('.previousvalue');
+const operatorButtons = document.querySelectorAll('.opt');
+const NumberButtons = document.querySelectorAll('.num');
+const clearButton = document.querySelector('.clear');
+const deleteButton = document.querySelector('.remove');
+const equalButton = document.querySelector('.equal');
+const pointButton = document.querySelector('.point');
 
 function addDecimalIntoScreen(e) {
   const newDecimal = e.target.textContent;
-  if (!currentValueElem.textContent.includes(".")) {
+
+  if (!currentValueElem.textContent.includes('.')) {
     currentValueElem.textContent += newDecimal;
-    resetFlag = false
 
+    resetFlag = false;
   }
-}
-
-function evaluate() {
-  if (operator === "") return null;
-  if (operator === "÷" && currentValueElem.textContent == "0")
-    alert("cant divide by zero");
-
-  secondOperand = currentValueElem.textContent;
-  previousValueElem.textContent = `${firstOperand} ${operator} ${secondOperand} =`;
-  currentValueElem.textContent = operate(operator, firstOperand, secondOperand);
-
-  resetFlag = true;
-  operator = "";
-  console.log(firstOperand);
-  console.log(operator);
-  console.log(secondOperand);
-}
-
-function addNumberIntoScreen(event) {
-  if (previousValueElem.textContent.includes("=")) {
-    previousValueElem.textContent = "";
-    resetCurrentValueElem();
-  }
-
-  const newInput = event.target.textContent;
-
-  if (currentValueElem.textContent === "0" || resetFlag)
-    resetCurrentValueElem();
-  currentValueElem.textContent += newInput;
-}
-
-function addOperatorIntoScreen(event) {
-  if (operator !== "") evaluate();
-  const newOperator = event.target.textContent;
-  firstOperand = currentValueElem.textContent;
-  operator = newOperator;
-  previousValueElem.textContent = `${firstOperand} ${newOperator}`;
-  resetFlag = true;
 }
 
 function resetCurrentValueElem() {
-  currentValueElem.textContent = "";
+  currentValueElem.textContent = '';
   resetFlag = false;
 }
 
 function clearScreen() {
-  firstOperand = "";
-  secondOperand = "";
-  operator = "";
-  currentValueElem.textContent = "0";
-  previousValueElem.textContent = "";
+  firstOperand = '';
+  secondOperand = '';
+  operator = '';
+  currentValueElem.textContent = '0';
+  previousValueElem.textContent = '';
 }
 
 function DeleteLatestInput() {
@@ -109,11 +59,64 @@ function operate(opt, a, b) {
   a = Number(a);
   b = Number(b);
 
-  if (opt === "+") return add(a, b);
-  if (opt === "-") return subtract(a, b);
-  if (opt === "×") return multiply(a, b);
-  if (opt === "÷" && b === 0) return null;
-  if (opt === "÷") return divide(a, b);
+  if (opt === '+') return add(a, b);
+  if (opt === '-') return subtract(a, b);
+  if (opt === '×') return multiply(a, b);
+  if (opt === '÷' && b === 0) return null;
+  if (opt === '÷') return divide(a, b);
 
   return null;
 }
+
+function evaluate() {
+  if (operator === '') return null;
+  if (operator === '÷' && currentValueElem.textContent === '0')
+    alert('cant divide by zero');
+
+  secondOperand = currentValueElem.textContent;
+  previousValueElem.textContent = `${firstOperand} ${operator} ${secondOperand} =`;
+  currentValueElem.textContent = operate(operator, firstOperand, secondOperand);
+
+  resetFlag = true;
+  operator = '';
+
+  return null;
+}
+
+function addOperatorIntoScreen(event) {
+  if (operator !== '') evaluate();
+
+  const newOperator = event.target.textContent;
+  firstOperand = currentValueElem.textContent;
+  operator = newOperator;
+
+  previousValueElem.textContent = `${firstOperand} ${newOperator}`;
+
+  resetFlag = true;
+}
+
+function addNumberIntoScreen(event) {
+  if (previousValueElem.textContent.includes('=')) {
+    previousValueElem.textContent = '';
+    resetCurrentValueElem();
+  }
+
+  const newInput = event.target.textContent;
+
+  if (currentValueElem.textContent === '0' || resetFlag)
+    resetCurrentValueElem();
+  currentValueElem.textContent += newInput;
+}
+
+NumberButtons.forEach((numberButton) => {
+  numberButton.addEventListener('click', addNumberIntoScreen);
+});
+
+operatorButtons.forEach((operatorButton) => {
+  operatorButton.addEventListener('click', addOperatorIntoScreen);
+});
+
+clearButton.addEventListener('click', clearScreen);
+deleteButton.addEventListener('click', DeleteLatestInput);
+equalButton.addEventListener('click', evaluate);
+pointButton.addEventListener('click', addDecimalIntoScreen);
